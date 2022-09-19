253a65c0dbc64d3e5550ed6407d8bbf2c59e8bf4
HairGMan <90013210+HairGMan@users.noreply.github.com>
2022-09-19 07:47:11 -0300

    Update Backpack.tf Fabricator Panel.user.js

--- Backpack.tf Fabricator Panel.user.js
+++ Backpack.tf Fabricator Panel.user.js
@@ -7,6 +7,8 @@
 // @match        https://backpack.tf/profiles/*
 // @iconURL      https://steamcdn-a.akamaihd.net/apps/440/icons/professional_kit_rare.97f580303b4d77bfe95dda36615979508a5b0533.png
 // @supportURL   https://github.com/HairGMan/bptf-fabricator-panel/issues
+// @downloadURL  https://github.com/HairGMan/bptf-fabricator-panel/raw/main/Backpack.tf%20Fabricator%20Panel.user.js
+// @updateURL    https://github.com/HairGMan/bptf-fabricator-panel/raw/main/Backpack.tf%20Fabricator%20Panel.meta.js
 // @grant        none
 // ==/UserScript==
 
